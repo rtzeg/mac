002db46b99fcecb38f7291fdebf2359fb80e443e
0Amin3 <aminfedya2888@gmail.com>
2023-03-15 16:36:03 +0500

    esc keycodev2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let white_btn  = document.querySelector('.btn-white')
+let white_btn = document.querySelector('.btn-white')
 let grey_btn = document.querySelector('.btn-grey')
 let btn1 = document.querySelector('.btn1')
 let btn2 = document.querySelector('.btn2')
@@ -13,87 +13,84 @@ let p2 = document.querySelector('.colors')
 
 
 
-let rem = (btn)=>{
-btn.classList.remove('btn1')
+let rem = (btn) => {
+    btn.classList.remove('btn1')
 }
 
-btn1.onclick = ()=>{
+btn1.onclick = () => {
     p.innerHTML = '$1,999'
     btn1.classList.add('btn1')
-        rem(btn2)
-        rem(btn3)
-        rem(btn4)
-    }
-    btn2.onclick = ()=>{
-        p.innerHTML = '$2,199'
-        btn2.classList.add('btn1')
-        rem(btn1)
-        rem(btn3)   
-        rem(btn4)
-    }
-    btn3.onclick = ()=>{
-        p.innerHTML = '$2,599'
-        btn3.classList.add('btn1')
-        rem(btn1)
-        rem(btn2)
-        rem(btn4)
-    }
-    btn4.onclick = ()=>{
-        p.innerHTML = '$3,199'
-        btn4.classList.add('btn1')
-        rem(btn1)
-        rem(btn3)
-        rem(btn2)
-    }
+    rem(btn2)
+    rem(btn3)
+    rem(btn4)
+}
+btn2.onclick = () => {
+    p.innerHTML = '$2,199'
+    btn2.classList.add('btn1')
+    rem(btn1)
+    rem(btn3)
+    rem(btn4)
+}
+btn3.onclick = () => {
+    p.innerHTML = '$2,599'
+    btn3.classList.add('btn1')
+    rem(btn1)
+    rem(btn2)
+    rem(btn4)
+}
+btn4.onclick = () => {
+    p.innerHTML = '$3,199'
+    btn4.classList.add('btn1')
+    rem(btn1)
+    rem(btn3)
+    rem(btn2)
+}
 
-    let img = document.querySelector('img')
-    let src1 = "./img/mac.png" 
-    let src2 = "./img/macGrey.png"
-
-
-
-    let rem2 = (btn)=>{
-        btn.classList.remove('btn-white')
-    }
+let img = document.querySelector('img')
+let src1 = "./img/mac.png"
+let src2 = "./img/macGrey.png"
 
 
-    grey_btn.onclick = ()=>{
-        img.setAttribute("src", src1)
+
+let rem2 = (btn) => {
+    btn.classList.remove('btn-white')
+}
+
+
+grey_btn.onclick = () => {
+    img.setAttribute("src", src1)
     p2.innerHTML = "Space Grey"
     grey_btn.classList.add('btn-white')
     rem2(white_btn)
-    }
+}
 
-    white_btn.onclick = ()=>{
-        img.setAttribute("src" , src2)
-        p2.innerHTML = "White"
-        white_btn.classList.add('btn-white')
-        rem2(grey_btn)
+white_btn.onclick = () => {
+    img.setAttribute("src", src2)
+    p2.innerHTML = "White"
+    white_btn.classList.add('btn-white')
+    rem2(grey_btn)
 
-    } 
+}
 
 let body = document.body
 let prices = document.querySelector('.prices')
 let modal_window = document.querySelector('.preload')
 let cros = document.querySelector('.btn-x')
-let modal = ()=>{
+let modal = () => {
     modal_window.classList.toggle('not_active')
     body.style.overflow = 'hidden'
 }
 prices.addEventListener("dblclick", modal)
-let modal2 = ()=>{
+let modal2 = () => {
     modal_window.classList.toggle('not_active')
     body.style.overflow = 'auto'
 }
 
 cros.addEventListener("click", modal2)
-
-
-
 window.addEventListener('keydown', (event) => {
-    if(event.keyCode === 27){
+    if (event.keyCode === 27) {
         modal_window.classList.toggle('not_active')
         body.style.overflow = 'auto'
     }
-    
+
 })
